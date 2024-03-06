b642e3ee85a17ba8bfbaa2db20a8e0028623c1fe
AliceGonzalez <alicegcodes@gmail.com>
2024-03-06 15:41:57 -0500

    2/3 String working

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,67 +1,87 @@
 package io.zipcoder;
 
 
+import java.util.Arrays;
+
+
 /**
  * @author tariq
  */
 public class StringsAndThings {
 
-    /**
-     * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count,
-     * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
-     * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
-     * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
-     */
-    public Integer countYZ(String input){
-        return null;
+//    /**
+//     * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count,
+//     * but not the 'y' in "yellow" (not case-sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
+//     * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+//     * example : countYZ("fez day"); // Should return 2
+//     *           countYZ("day fez"); // Should return 2
+//     *           countYZ("day fyyyz"); // Should return 2
+//     */
+////    You will be using the following concepts:
+////    Using the String class
+////    Using the StringBuilder class
+////    Using the System.out.println() method
+////    There are a lot of methods in theString class. You will need to use the Java API
+////    Zip Code Wilmington, Copyright © 2024 - All Rights Reserved.
+////    documentation to find the methods you need to use. You will also need to use the StringBuilder class to create the output. You will need to use the System.out.print() method to print out the numbers in the correct format. What’s the “newline” character? You will need to use it to get the output to look like the example.
+    public Integer countYZ(String input) {
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = Character.toLowerCase(input.charAt(i));
+            if(currentChar == 'y'  || currentChar == 'z'  &&
+                    (i == input.length() -1 || ! Character.isLetter(input.charAt(i +1)))){
+                count++;
+            }
+        }
+        return count;
     }
 
-    /**
-     * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
-     * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
-     * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
-     * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
-     */
+//    /**
+//     * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
+//     * been removed (not case-sensitive). You may assume that the remove string is length 1 or more.
+//     * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+//     *
+//     * example : removeString("Hello there", "llo") // Should return "He there"
+//     *           removeString("Hello there", "e") //  Should return "Hllo thr"
+//     *           removeString("Hello there", "x") // Should return "Hello there"
+//     */
     public String removeString(String base, String remove){
+        String result = "";
+
         return null;
     }
 
-    /**
-     * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
-     * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
-     * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
-     */
+//    /**
+//     * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
+//     * to the number of appearances of "not" anywhere in the string (case-sensitive)
+//     *
+//     * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
+//     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+//     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+//     */
     public Boolean containsEqualNumberOfIsAndNot(String input){
         return null;
     }
 
-    /**
-     * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
-     * Return true if all the g's in the given string are happy.
-     * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
-     */
+//    /**
+//     * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+//     * Return true if all the g's in the given string are happy.
+//     * example : gHappy("xxggxx") // Should return  true
+//     *           gHappy("xxgxx") // Should return  false
+//     *           gHappy("xxggyygxx") // Should return  false
+//     */
     public Boolean gIsHappy(String input){
         return null;
     }
 
 
-    /**
-     * We'll say that a "triple" in a string is a char appearing three times in a row.
-     * Return the number of triples in the given string. The triples may overlap.
-     * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
-     */
+//    /**
+//     * We'll say that a "triple" in a string is a char appearing three times in a row.
+//     * Return the number of triples in the given string. The triples may overlap.
+//     * example :  countTriple("abcXXXabc") // Should return 1
+//     *            countTriple("xxxabyyyycd") // Should return 3
+//     *            countTriple("a") // Should return 0
+//     */
     public Integer countTriple(String input){
         return null;
     }
